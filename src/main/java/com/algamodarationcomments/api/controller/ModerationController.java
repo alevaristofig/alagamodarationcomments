8a509e4f4259d9281eed8e3209fe4f08e31414c4
Alexandre <alevaristofig@gmail.com>
2025-07-24 17:50:23 -0300

    Implementação do metodo moderate para moderar os comentarios no controller

--- src/main/java/com/algamodarationcomments/api/controller/ModerationController.java
+++ src/main/java/com/algamodarationcomments/api/controller/ModerationController.java
@@ -1,5 +1,6 @@
 package com.algamodarationcomments.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algamodarationcomments.api.model.ModerationInput;
 import com.algamodarationcomments.api.model.ModerationOutput;
+import com.algamodarationcomments.domain.service.ModerationService;
 
 @RestController
 @RequestMapping(path = "/api/moderate")
 public class ModerationController {
+	
+	@Autowired
+	private ModerationService service;
 
 	@PostMapping
-	public /*ModerationOutput*/void moderation(@RequestBody ModerationInput input) {
-		
+	public ModerationOutput moderation(@RequestBody ModerationInput input) {
+		return service.moderate(input);
 	}
 }
