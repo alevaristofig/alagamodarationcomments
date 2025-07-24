640b3627887c87002ce84adc07c331245864e9c9
Alexandre <alevaristofig@gmail.com>
2025-07-24 17:49:09 -0300

    Implementação do metodo moderate para moderar os comentarios

--- src/main/java/com/algamodarationcomments/api/model/ModerationOutput.java
+++ src/main/java/com/algamodarationcomments/api/model/ModerationOutput.java
@@ -1,8 +1,14 @@
 package com.algamodarationcomments.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ModerationOutput {
 
 	private Boolean approved;
--- src/main/java/com/algamodarationcomments/domain/service/ModerationService.java
+++ src/main/java/com/algamodarationcomments/domain/service/ModerationService.java
@@ -1,8 +1,40 @@
 package com.algamodarationcomments.domain.service;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
+
+import com.algamodarationcomments.api.model.ModerationInput;
+import com.algamodarationcomments.api.model.ModerationOutput;
 
 @Service
 public class ModerationService {
 
+	private static final List<String> BANNED_WORDS = List.of("ódio", "xingamentos", "idiotas");
+	
+	public ModerationOutput moderate(ModerationInput input) {
+		String text = input.getText().toLowerCase();
+		
+		Set<String> bannedWordsFound = new HashSet<>();
+		
+		for(String word: BANNED_WORDS) {
+			if(text.contains(word.toLowerCase())) {
+				bannedWordsFound.add(word);
+			}
+		}
+		
+		if(!bannedWordsFound.isEmpty()) {
+			return ModerationOutput.builder()
+					.approved(false)
+					.reason("Contém palavras proibidas" + bannedWordsFound)
+					.build();
+		}
+		
+		return ModerationOutput.builder()
+				.approved(true)
+				.reason("Comentário aprovado")
+				.build();
+	}
 }
