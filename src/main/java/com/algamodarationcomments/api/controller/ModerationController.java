14ab790f5a2920320c6aa3a550be700942e315df
Alexandre <alevaristofig@gmail.com>
2025-07-24 15:47:25 -0300

    Implementação do controller ModerationController

--- src/main/java/com/algamodarationcomments/api/controller/ModerationController.java
+++ src/main/java/com/algamodarationcomments/api/controller/ModerationController.java
@@ -1,5 +1,19 @@
 package com.algamodarationcomments.api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algamodarationcomments.api.model.ModerationInput;
+import com.algamodarationcomments.api.model.ModerationOutput;
+
+@RestController
+@RequestMapping(path = "/api/moderate")
 public class ModerationController {
 
+	@PostMapping
+	public /*ModerationOutput*/void moderation(@RequestBody ModerationInput input) {
+		
+	}
 }
